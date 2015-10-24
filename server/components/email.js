91b1c538031cc32d53ed909c2cd49ed7d55a0b00
John Kim <yohany@gmail.com>
2015-10-24 13:15:22 +0900

    fix some sources on mail sender (configurable )

--- server/components/email.js
+++ server/components/email.js
@@ -2,64 +2,33 @@ var nodemailer = require('nodemailer');
 var config = require('./../config/environment');
 var _ = require('lodash');
 
-var transport = nodemailer.createTransport("SMTP", {
-    host: "smtp.naver.com", // hostname
-    secureConnection: true, // use SSL
-    port: 465, // port for secure SMTP
-    auth: {
-        user: "nixenic@naver.com",
-        pass: "1q2w3e4r"
+var smtpTransport = nodemailer.createTransport("SMTP", config.email.smtc);
+
+var sendVerifyMail = function (name, emailAddress, authId) {
+
+  var mailContents = config.email.verify.options; // from, subject, text
+
+  mailContents['to'] = emailAddress;
+  mailContents['text'] = mailContents['text'] + '\n' +
+    config.email.verify.callbackUrl + name + "/" + emailAddress + "/" + authId;
+
+
+  console.log(config.email.smtc, mailContents);
+
+  smtpTransport.sendMail(mailContents, function (error, response) {
+
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Message sent : " + response.message);
     }
-});
+    smtpTransport.close();
+
+  });
 
 
-var smtpTransport = nodemailer.createTransport("SMTP", {
-    service: 'Gmail',
-    auth: {
-        user: "todos.platform@gmail.com",
-        pass: 'qwerLKJH1'
-    }
-});
-
-/*
-mailer.extend(app, {
-  from: 'todos.platform@gmail.com',
-  host: 'smtp.gmail.com', // hostname
-  secureConnection: true, // use SSL
-  port: 465, // port for secure SMTP
-  transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
-  auth: {
-    user: 'todos.platform@gmail.com',
-    pass: 'qwerLKJH1'
-  }
-});
-*/
-var mailOptions = {
-    from: '백부석 <nixenic@naver.com>',
-    to: 'nixenic@gmail.com',
-    subject: 'Withtalk 에서 보내는 인증 메일',
-    text: '평문 보내기 테스트 '
 };
 
-var withtalkContents = '다음의 링크를 클릭해서 계정을 활성화 해주세요. '
-withtalkContents += 'http://localhost:9000/signup/';
-
-var sendMail = function(name, emailAddress , contents){
-    var mailContents = _.merge(mailOptions, {
-        to: emailAddress,
-        text: withtalkContents+name+"/"+emailAddress+"/" + contents
-    });
-    transport.sendMail(mailContents, function(error, response){
-
-        if (error){
-            console.log(error);
-        } else {
-            console.log("Message sent : " + response.message);
-        }
-        smtpTransport.close();
-    });
-}
-
 module.exports = {
-  sendMail : sendMail  
+  sendVerifyMail: sendVerifyMail
 }
--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,13 +1,33 @@
 'use strict';
 
 module.exports = {
+
   // MongoDB connection options
   mongo: {
     uri: 'mongodb://127.0.0.1/withtalk-dev'
   },
+
   email: {
-    	id : "todos.platform@gmail.com",
-    	pass: "qwerLKJH1"
+
+    smtc: {
+      host: "[SMTC Hostname]", // hostname
+      secureConnection: true, // use SSL
+      port: 465, // port for secure SMTP
+      auth: {
+        user: "[USER]",
+        pass: "[PASSWORD]"
+      }
+    },
+
+    verify: {
+      options: {
+        from: "contact@yourcampany.com",
+        subject: "Verify Your Email on LINK Service",
+        text: "To complete your sign up, please verify your email using the following link : \n"
+      },
+      callbackUrl: 'http://localhost:9000/signup/'
+    }
+
   },
 
   seedDB: true
