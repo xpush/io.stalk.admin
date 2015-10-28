afdc648acd07c201f70fd19203a1385b85bc6b17
EunsanKo <eunsan.ko@gmail.com>
2015-10-28 20:52:54 +0900

    add images & modify dashboard

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,13 +1,33 @@
 'use strict';
 
 angular.module('withtalkApp')
-  .controller('DashboardCtrl', function ($rootScope, $scope, Auth) {
+  .controller('DashboardCtrl', function ($rootScope, $scope, Auth, $interval) {
     $rootScope.isLogin=false;
     $scope.message = 'Hello';
 
     $scope.getCurrentUser = Auth.getCurrentUser;
 
+
+    $scope.current_visitor=0;
+    $scope.current_operators=0;
+    $scope.current_chatting=0;
+    $scope.current_waiting=0;
+
+    var dashboard = $interval(function(){
+      $scope.current_visitor += 1;
+      $scope.current_operators+=1;
+      $scope.current_chatting+=2;
+      $scope.current_waiting+=3;
+
+      if($scope.current_visitor==80){
+        $interval.cancel(dashboard);
+      }
+    },10);
+
+
+
     $scope.sendNotificaton = function(){
+
       alert(CKEDITOR.instances['editor1'].getData());
     };
 
