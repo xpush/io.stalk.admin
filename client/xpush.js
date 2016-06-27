c849171c9968a3b982f8ed547e8449a69080a518
0nlyoung7 <0nlyoung7@gmail.com>
2016-06-27 19:23:30 +0900

    Add link in linking type message

--- client/xpush.js
+++ client/xpush.js
@@ -901,6 +901,10 @@
     XPush.prototype.send = function(channel, name, data){
       var self = this;
 
+      if( !data.TP && UTILS.validateURL( data.MG ) ){
+        data.TP = 'LN';
+      }
+
       self.getChannelAsync(channel, function (err, ch){
         ch.send(name,data);
       });
@@ -1727,6 +1731,12 @@
         delete data[key];
       }
     };
+
+
+    UTILS.validateURL = function(textval) {
+      var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+      return urlregex.test(textval) || urlregex.test( decodeURIComponent(textval) );
+    }
 
     return XPush;
   })();
