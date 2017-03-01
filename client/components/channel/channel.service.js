b02625bb29e8fd75590680e7649ddf285b57eeec
0nlyoung7 <0nlyoung7@gmail.com>
2017-03-01 16:20:26 +0900

    bug fix in chatService

--- client/components/channel/channel.service.js
+++ client/components/channel/channel.service.js
@@ -29,51 +29,45 @@ angular.module('stalkApp')
 
         $http.post('/api/channels/close',
             channel
-          ).
-          success(function (data) {
-            deferred.resolve(data);
-            return cb();
-          }).
-          error(function (err) {
-            deferred.reject(err);
-            return cb(err);
-          }.bind(this));
+        ).then(function successCallback(response) {
+              deferred.resolve(response.data);
+              return cb();
+        }, function errorCallback(response) {
+              deferred.reject(response);
+              return cb(response);
+        });
 
         return deferred.promise;
-      },      
+      },
       list : function(callback){
         var cb = callback || angular.noop;
         var deferred = $q.defer();
-        
+
         $http.post('/api/channels',
           {}
-          ).
-          success(function (data) {
-            deferred.resolve(data);
-            return cb();
-          }).
-          error(function (err) {
-            deferred.reject(err);
-            return cb(err);
-          }.bind(this));
+        ).then(function successCallback(response) {
+              deferred.resolve(response.data);
+              return cb();
+        }, function errorCallback(response) {
+              deferred.reject(response);
+              return cb(response);
+        });
 
         return deferred.promise;
       },
       search : function(param, callback){
         var cb = callback || angular.noop;
         var deferred = $q.defer();
-        
+
         $http.post('/api/channels/search',
           param
-          ).
-          success(function (data) {
-            deferred.resolve(data);
-            return cb();
-          }).
-          error(function (err) {
-            deferred.reject(err);
-            return cb(err);
-          }.bind(this));
+        ).then(function successCallback(response) {
+              deferred.resolve(response.data);
+              return cb();
+        }, function errorCallback(response) {
+              deferred.reject(response);
+              return cb(response);
+        });
 
         return deferred.promise;
       }
